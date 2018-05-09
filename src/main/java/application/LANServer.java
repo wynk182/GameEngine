9b5f0abb1ba02a983ed147b4ea4640cdbc1cfef3
Michael <wynk182@gmail.com>
2018-05-08 22:04:59 -0700

    bedtime

--- src/main/java/application/LANServer.java
+++ src/main/java/application/LANServer.java
@@ -3,9 +3,13 @@ package application;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 import javafx.concurrent.Service;
@@ -24,19 +28,17 @@ public class LANServer extends Service<Void>{
             protected Void call() throws Exception {
             	//Thread.sleep(time *1000);
             	while(true) {
-					ServerSocket s = new ServerSocket(1337);
-					Socket game = s.accept();
-					BufferedReader in = new BufferedReader(new InputStreamReader(
-                            game.getInputStream()));
-					String line = "";
-					while((line = in.readLine()) != null) {
-						System.out.println(line);
-					}
-						
-					game.close();
-					s.close();
-					
-				}
+            		ServerSocket s = new ServerSocket(1337);
+            		Socket game = s.accept();
+            		BufferedReader in = new BufferedReader(new InputStreamReader(game.getInputStream()));
+            		System.out.println(in.readLine());
+            		System.out.println(in.readLine());
+            		PrintWriter out = new PrintWriter(game.getOutputStream());
+            		out.write("end");
+            		out.close();
+            		game.close();
+            		s.close();
+            	}
             }
         };
 	}
