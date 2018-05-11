33a2b766b491f2425c26a33c504cd4a55690fe89
michaelw <michaelw@lavi.com>
2018-05-11 13:37:03 -0700

    die

--- src/main/java/application/Character.java
+++ src/main/java/application/Character.java
@@ -5,7 +5,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import javafx.scene.Cursor;
-import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
